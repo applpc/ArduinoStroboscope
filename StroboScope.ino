fd2cfe7288a6482db46408e3d49289dd2eef3b36
applpc <kennyevanadams@gmail.com>
2017-10-16 19:35:44 -0400

    Add files via upload

---
+++ StroboScope.ino
@@ -0,0 +1,31 @@
+//Stroboscope Code
+//Kenny Adams
+//10/13/17
+#define mosfetPin 11;
+#define upButtonPin 2;
+#define downButtonPin 3;
+
+int waitTime = 10; //ms
+int freq = (1000/waitTime); // stropes/s
+
+void setup() {
+  pinMode(mosfetPin, OUTPUT);
+  pinMode(upButtonPin, INPUT);
+  pinMode(downButtonPin, INPUT);
+}
+
+void loop() {
+  upButtonState = digitalRead(upButtonPin);
+  downButtonState = digitalRead(downButtonPin);
+  if(upButtonState == HIGH){
+    waitTime++;
+  }
+  if(downButtonState == HIGH){
+    waitTime--;
+  }
+  digitalWrite(mosfetPin, HIGH);
+  delay(waitTime);
+  digitalWrite(mosfetPin, LOW);
+  delay(waitTime);
+  
+}
