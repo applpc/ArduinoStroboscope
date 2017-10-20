66668cd489d373e7c53e2c77282b47ec487eef54
applpc <kennyevanadams@gmail.com>
2017-10-20 15:37:09 -0400

    Update StroboScope.ino

--- StroboScope.ino
+++ StroboScope.ino
@@ -1,28 +1,41 @@
 //Stroboscope Code
 //Kenny Adams
 //10/13/17
-#define mosfetPin 11;
-#define upButtonPin 2;
-#define downButtonPin 3;
+//Rev. 10/20/17
+#define mosfetPin 11
+#define upButtonPin 9
+#define downButtonPin 8
 
-int waitTime = 10; //ms
-int freq = (1000/waitTime); // stropes/s
+int freq = 100; // stropes/s
+int waitTime = (1000/freq); //ms
+int upButtonState;
+int downButtonState;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(mosfetPin, OUTPUT);
-  pinMode(upButtonPin, INPUT);
-  pinMode(downButtonPin, INPUT);
+  pinMode(upButtonPin, INPUT_PULLUP);
+  pinMode(downButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
+  Serial.println(freq);
   upButtonState = digitalRead(upButtonPin);
   downButtonState = digitalRead(downButtonPin);
-  if(upButtonState == HIGH){
-    waitTime++;
+  if(upButtonState == LOW){
+    freq = freq + 5;
+    delay(100);
   }
-  if(downButtonState == HIGH){
-    waitTime--;
+  if(downButtonState == LOW){
+    if(freq > 5){
+      freq = freq - 5;
+      delay(100);
+    } else{
+      freq = 100;
+      delay(100);
+    }
   }
+  waitTime = (1000/freq);
   digitalWrite(mosfetPin, HIGH);
   delay(waitTime);
   digitalWrite(mosfetPin, LOW);
